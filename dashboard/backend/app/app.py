72da19c3e1d3ad39748715d766df4ec097610bb9
Cookieheheboiz <danghuy2498@gmail.com>
2026-05-19 16:38:17 +0700

    style: format CORS middleware allow_origins for cicd build/deploy

--- dashboard/backend/app/app.py
+++ dashboard/backend/app/app.py
@@ -38,7 +38,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:8000", "http://34.61.176.172:5173"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:8000",
+        "http://34.61.176.172:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
